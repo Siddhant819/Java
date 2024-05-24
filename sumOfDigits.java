706cf87331827f00b367ac100b6f8b23c363616d
Siddhant Acharya <connect.siddhant81@gmail.com>
2024-05-24 23:26:17 +0545

    Add files via upload

---
+++ sumOfDigits.java
@@ -0,0 +1,24 @@
+package day5;
+
+import java.util.Scanner;
+
+public class sumOfDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Sum Of Digits");
+        System.out.print("Please enter your number: ");
+        int num = input.nextInt();
+        int sum = sumOfDigits(num);
+        System.out.println("Sum of Digits is: " + sum);
+    }
+
+    public static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+    
+}
