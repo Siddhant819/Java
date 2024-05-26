11453a2c11e7804ac0689ad14df8c53b3483ceb8
Siddhant Acharya <connect.siddhant81@gmail.com>
2024-05-26 23:26:59 +0545

    Add files via upload

---
+++ clearbit.java
@@ -0,0 +1,13 @@
+package day6;
+
+public class clearbit {
+    public static void main(String[] args) {
+        int n = 5; 
+     int pos = 2;
+     int bitMask = 1<<pos;
+     int newBitMask = ~(bitMask);
+     int newNumber = newBitMask & n;
+     System.out.println(newNumber);
+    }
+    
+}
---
+++ setbit.java
@@ -0,0 +1,14 @@
+package day6;
+import java.util.*;
+public class setbit {
+    public static void main(String args[]) {
+        int n = 5; 
+        int pos = 1;
+        int bitMask = 1<<pos;
+  
+  
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+     }
+  
+}
